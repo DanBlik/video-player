e19cd158f1728163926a44a33db11e4edcfbb93b
Бликов Данила Дмитриевич <DDBlikov@sberbank.ru>
2022-02-01 18:56:59 +0300

    add template

---
+++ script.js
@@ -0,0 +1,21 @@
+const video = document.querySelector('.video')
+
+// Play & Pause ----------------------------------- //
+
+
+
+// Progress Bar ---------------------------------- //
+
+
+
+// Volume Controls --------------------------- //
+
+
+
+// Change Playback Speed -------------------- //
+
+
+
+// Fullscreen ------------------------------- //
+
+
