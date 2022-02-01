070d1ad314b59ebe5e45be6961b241ea6b80f5ae
Бликов Данила Дмитриевич <DDBlikov@sberbank.ru>
2022-02-01 19:41:06 +0300

    add duration and progress

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 const video = document.querySelector('.video')
+const progressRange = document.querySelector('.progress-range')
+const progressBar = document.querySelector('.progress-bar')
+const playBtn = document.querySelector('#play-btn')
+const volumeIcon = document.querySelector('#volume-icon')
+const volumeRange = document.querySelector('.volume-range')
+const volumeBar = document.querySelector('.volume-bar')
+const currentTime = document.querySelector('.time-elapsed')
+const duration = document.querySelector('.time-duration')
+const fullScreenBtn = document.querySelector('.fullscreen')
+
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon() {
+  playBtn.classList.replace('fa-pause', 'fa-play')
+  playBtn.setAttribute('title', 'Play')
+}
 
+function togglePlay() {
+  if (video.paused) {
+    video.play()
+    playBtn.classList.replace('fa-play', 'fa-pause')
+    playBtn.setAttribute('title', 'Pause')
+  } else {
+    video.pause()
+    showPlayIcon()
+  }
+}
 
 
 // Progress Bar ---------------------------------- //
 
+function displayTime(time) {
+  const minutes = Math.floor(time / 60)
+  let seconds = Math.floor(time % 60)
+  seconds = seconds < 10 ? `0${seconds}` : seconds
 
+  return `${minutes}:${seconds}`
+}
+
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration ) * 100}%`
+  currentTime.textContent = `${displayTime(video.currentTime)} /`
+  duration.textContent = `${displayTime(video.duration)}`
+}
 
 // Volume Controls --------------------------- //
 
@@ -19,3 +55,9 @@ const video = document.querySelector('.video')
 // Fullscreen ------------------------------- //
 
 
+// Listeners
+playBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
+video.addEventListener('ended', showPlayIcon)
+video.addEventListener('timeupdate', updateProgress)
+video.addEventListener('canplay', updateProgress)
